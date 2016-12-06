b5e7a9d219fa7e8388d9765775d82d5895c5f59d
shereif <sas664@scarletmail.rutgers.edu>
2016-12-06 18:14:02 -0500

    Memory Block is now filled in

--- y86.py
+++ y86.py
@@ -1,37 +1,40 @@
-#Author Shereif Saleh
-#Y86 - Emulator
+# Shereif Saleh
+# y86.py
+# Rutgers University Computer Architecture
+
 class emulate():
-    def __init__(self,file,size = 0,text = 0,byte = None):
-        try: # Attempt read the file
+    def __init__(self,file,size = 0,text = 0,byte = None, long = None):
+        try:                       # Attempt read the file
             file_object = open(file,'r')
             self.file = file_object
-        except IOError: # Raise exception if error
+        except IOError:            # Raise exception if error
             print("Wrong file or file path")
             return
         
-        seq = ''
-        
-        #for line in file_object:
-#            if line[0:5] == '.byte':
-#                byte = ''
-#                byte += line
-#        self.byte = list(byte)
+        BYTE = ''
+        LONG = ''
         
         for i, line in enumerate(file_object):
             if line[0:5] == '.size':
-                SIZE = line      # Set self.text
+                SIZE = line        # Set self.text
                 self.size = SIZE
             if line[0:5] == '.text':
-                TEXT = line      # Set self.size
+                TEXT = line        # Set self.size
                 self.text = TEXT
-        
+            if line[0:5] == '.byte':
+                BYTE += line
+            if line[0:5] == '.long':
+                LONG += line
                 
-    def getRegisterInit(self): # Gives the Register Initial Size
-        temp = self.text # Copy of self.text
+        self.byte = BYTE
+        self.long = LONG
+                
+    def getRegisterInit(self):     # Gives the Register Initial Size
+        temp = self.text           # Copy of self.text
         return int(temp.partition('\t')[2].rpartition('\t')[0]) #Return my register initial size
 
     def getTextInstructions(self):
-        temp = self.text # Copy of self.text
+        temp = self.text           # Copy of self.text
         NewInstructions = ''
         PreFilteredInstructions = temp.partition('\t')[2].rpartition('\t')[2]
         
@@ -40,26 +43,63 @@ class emulate():
                 NewInstructions += i
         return NewInstructions
     
-    def getSize(self): # Gets me the Size of my Register
+    def getSize(self):              # Gets me the Size of my Register
         num = ['0','1','2','3','4','5','6','7','8','9']
         mySize = ''
         temp = self.size
         self.temporarySIZE = ''
+        
         for i in temp:              # Adjust for formatting
             if i != '\t' and i != '\n':
                 self.temporarySIZE += i
             elif i == '\t' or i == '\n':
                 self.temporarySIZE += ' '
+                
         for i in self.temporarySIZE: # Find my number
             if i in num:
                 mySize += i
                 
         self.size = int(mySize,16)   # Change my size into an an Integer
         return self.size
-
+    
     def getByte(self):
-        print(self.byte)
-    # def getLong(self): This must be implemented to get long from a file
-#if __name__ == '__main__':
-#    e = emulate('prog2.y86.txt')
+        myByte = ''
+        longByte = []
+        splitByte = self.byte.split()
+        for i in range(1,len(splitByte),1):
+            longByte.append(splitByte[i])
+
+        longByte = [x for x in longByte if x != '.byte'] # Remove .byte
+
+        final = [[int(longByte[i],16),longByte[i+1]] for i in range(0,len(longByte),2)]
+        
+        self.byte = final
+        if self.byte == [ ]:
+            return None
+        
+        return self.byte
+    
+    def getLong(self):
+        myLong = ''
+        longList = [ ]
+        splitLong = self.long.split()
+        for i in range(1,len(splitLong),1):
+                longList.append(splitLong[i])
+
+        longList = [x for x in longList if x != '.long'] # Remove .long
+
+        final = [[int(longList[i],16),longList[i+1]]for i in range(0,len(longList),2)]
+        
+        self.long = final
+        if self.long == [ ]:
+            return None
+        
+        return self.long
+    
+if __name__ == '__main__':
+    e = emulate('prog1.y86.txt')
+    print(e.getTextInstructions())
+#    print(e.getByte())
+#    print(e.getLong())
+
     
--- y86emul.py
+++ y86emul.py
@@ -1,16 +1,50 @@
-#Author Shereif Saleh
-#Y86 - Emulator
+# Shereif Saleh
+# y86emul.py
+# Rutgers University Computer Architecture
+
 import y86
+import sys
+
+inputfile = sys.argv[1]
+
 
 global Emulator
-Emulator = y86.emulate('prog1.y86.txt')
-
+Emulator = y86.emulate(inputfile)
 
 def CreateMemoryBlock():
     MemoryBlock = [Emulator.getRegisterInit()]*Emulator.getSize() # Create my list aka 'MemoryBlock'
     return MemoryBlock
 
+def FillInMemoryBlock(Block): # Probably requires a rework but algo worsks
+    Long = Emulator.getLong()
+    Byte = Emulator.getByte()
+    counter = 0
+    try:
+        for i in Long:
+            Block[i[0]] = i[1]
+    except:
+        print("No long detected")
+        
+    try:
+        for j in Byte:
+            Block[j[0]] = j[1]
+            counter += 1
+        print(counter)
+    except:
+        print("No long detected")
+        
+    return Block
 
+
+def PrintMemoryBlock(Block):
+    print(Block)
+
+def Instructions():
     
+    return Emulator.getTextInstructions()
+
 if __name__ == '__main__':
-    print(CreateMemoryBlock())
+    #MemoryBlock = CreateMemoryBlock()
+    #FillInMemoryBlock(MemoryBlock)
+    #PrintMemoryBlock(MemoryBlock)
+    print(Instructions())
